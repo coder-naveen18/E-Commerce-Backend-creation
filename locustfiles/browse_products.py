6823472d15e19635cc3d36e244ad1b7857cceb9f
coder-naveen18 <codersahu18@gmail.com>
2026-01-03 22:15:27 +0530

    fix: Correct endpoint name for adding items to the cart in locust tasks

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -25,7 +25,7 @@ class WebsiteUser(HttpUser):
         product_id = randint(1,10)
         self.client.post(
                         f'/store/carts/{self.cart_id}/items/',
-                        name='/store/carts/:items', 
+                        name='/store/carts/items', 
                         json={'product_id': product_id, 'quantity': 1}
                         )
 
